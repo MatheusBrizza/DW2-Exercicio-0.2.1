813a2ff7d249b7551a9681d8a9f52768460957d2
MatheusBrizza <matheusbj95@gmail.com>
2022-04-29 21:54:10 -0300

    index corrigido, corrigido listaFilmesBatman de objeto para array, retirado os objetos batman, batmanCavaleiro e batmanCavaleiroRessurge de dentro de listaFilmesBatman e transformados em objetos próprios.

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const listaFilmesBatman = {
-   batmanBegins = {
-      id: 01,
-      genero: 'heroi',
-      atores: ['Christian Bale', 'Michael Cane', 'Cillian Murphy', 'Gary Oldman'],
-      diretor: 'Christofer Nolan',
-      faixaEtaria: 16,
-      duracaoMinutos: 120
-  },
-   batmanCavaleiro = {
-     id: 02, 
-     genero: 'heroi',
-     atores: ['Christian Bale', 'Michael Cane', 'Heath Ledger', 'Gary Oldman'],
-     diretor: 'Christofer Nolan',
-     faixaEtaria: 16,
-     duracaoMinutos: 132
-  },
-   batmanCavaleiroRessurge = {
-     id: 03,
-     genero: 'heroi',
-     atores: ['Christian Bale', 'Michael Cane', 'Tom Hardy', 'Gary Oldman', 'Anne Hathaway'],
-     diretor: 'Christofer Nolan',
-     faixaEtaria: 16,
-     duracaoMinutos: 145
-   }
-}
+const batmanBegins = {
+  id: 01,
+  genero: ['ação', 'heroi'],
+  atores: ['Christian Bale', 'Michael Cane', 'Cillian Murphy', 'Gary Oldman'],
+  diretor: 'Christofer Nolan',
+  faixaEtaria: 16,
+  duracaoMinutos: 120,
+};
 
+const batmanCavaleiro = {
+  id: 02,
+  genero: ['ação', 'heroi'],
+  atores: ['Christian Bale', 'Michael Cane', 'Heath Ledger', 'Gary Oldman'],
+  diretor: 'Christofer Nolan',
+  faixaEtaria: 16,
+  duracaoMinutos: 132,
+};
+
+const batmanCavaleiroRessurge = {
+  id: 03,
+  genero: ['ação','heroi'],
+  atores: [
+    'Christian Bale',
+    'Michael Cane',
+    'Tom Hardy',
+    'Gary Oldman',
+    'Anne Hathaway',
+  ],
+  diretor: 'Christofer Nolan',
+  faixaEtaria: 16,
+  duracaoMinutos: 145,
+};
+
+const listaFilmesBatman = [
+  batmanBegins,
+  batmanCavaleiro,
+  batmanCavaleiroRessurge,
+];
